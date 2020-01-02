946919323cff110490f4fce1629a203b95131d14
Chris Northwood <chris@pling.org.uk>
2020-01-02 21:51:13 +0000

    update personal website

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'bourbon', '~> 4.2.2'
 gem 'compass'
-gem 'neat'
+gem 'neat', '~> 1.9.1'
 gem 'scss_lint'
--- Gruntfile.js
+++ Gruntfile.js
@@ -57,10 +57,6 @@ module.exports = function(grunt) {
                 files: ['Gemfile'],
                 tasks: ['bootstrap-compass']
             },
-            bower: {
-                files: ['bower.json'],
-                tasks: ['bower']
-            },
             styles: {
                 files: ['assets/scss/compass_config.rb', 'assets/scss/**/*.scss'],
                 tasks: ['styles-dev']
@@ -82,16 +78,12 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-scss-lint');
 
-    grunt.registerTask('bower', function() {
-        shelljs.exec('node_modules/bower/bin/bower install');
-    });
-
     grunt.registerTask('bootstrap-compass', function() {
         shelljs.exec('bundle install');
     });
 
-    grunt.registerTask('default', ['bower', 'bootstrap-compass', 'copy:favicon', 'copy:svg', 'imagemin:images', 'styles']);
-    grunt.registerTask('dev', ['bower', 'bootstrap-compass', 'copy:favicon', 'copy:svg', 'imagemin:images', 'styles-dev', 'watch']);
+    grunt.registerTask('default', ['bootstrap-compass', 'copy:favicon', 'copy:svg', 'imagemin:images', 'styles']);
+    grunt.registerTask('dev', ['bootstrap-compass', 'copy:favicon', 'copy:svg', 'imagemin:images', 'styles-dev', 'watch']);
 
     grunt.registerTask('styles', ['compass:compile', 'scsslint:all']);
     grunt.registerTask('styles-dev', ['compass:dev', 'scsslint:all']);
